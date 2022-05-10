778f8d54c8d243e1941ebdbcb51a298917f66940
sheyko <sheyko.wk@gmail.com>
2022-05-09 20:44:38 -0500

    declare the first redux thunk function

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,16 +1,33 @@
+import axios from "axios"
+import { getProducts } from "../../services"
+
 export const actions =  {
     productSetAll : "@product/setAll"
 }
 // dispatch({type: "@product/setAll", payload: data})
 
+/*  
+! 1.Ejecutamos el dispatch (el dispatch actualiza nuestro estado global)
+? dispatch({type: "@product/setAll", payload: data})
+
+! 2.Utilizando un middleware, vamos a manipular la informacion que vamos a setear en nuestro payload
+*/
+
+
 export const productSetAll = (data) => ({
     type : actions.productSetAll,
     payload: data
 })
-// dispatch(productSetAll())
 
-export const productSetById = (id) => ({
-    type : actions.productSetAll,
-    payload: id
-})
+export const setProductThunk = () => {
+    return (dispatch) => {
+        getProducts()
+            .then((res) => {
+                dispatch(productSetAll(res))
+            })
+    }
+}
+
+
+// dispatch(productSetAll())
 // dispatch(productSetById(data))
